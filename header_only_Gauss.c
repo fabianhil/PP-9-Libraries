a2c3c90280280cc58db67205d9e68582c85a3204
Fabian Hilbich <fabian_hilbich@web.de>
2022-06-26 21:57:00 +0200

    Programm zur Normalverteilung begonnen. Fabian

--- header_only_Gauss.c
+++ header_only_Gauss.c
@@ -1,3 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"headerlib.h"
+
+
+int main (){
+
+	print_gaussian_dist(int expected_value, int std_deviation, int amount_of_values, FILE * output_stream);
+
+return 0;
 
 
 
