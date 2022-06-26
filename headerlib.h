fc3eb67699fb1952a4c219059555b08630cbe421
Fabian Hilbich <fabian_hilbich@web.de>
2022-06-26 22:34:21 +0200

    Grundgerüst für Funktion. Fabian

--- headerlib.h
+++ headerlib.h
@@ -1,4 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 
+void print_gaussian_dist(int expected_value, int std_deviation, int amount_of_values, FILE * output_stream){
+	Formel HIER
 
-
-
+		AUSGABE IN DATEI HIER
+}
