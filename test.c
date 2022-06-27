aa754bbbc3792a9ce5fb64fc6e4936fcb7ebfc1a
Fabian Hilbich <fabian_hilbich@web.de>
2022-06-27 12:04:48 +0200

    Versuch Write to file zu implementieren. Kompilieren mit zusatz -lm löst linker problem mit math.h funktionen. Fabian

--- test.c
+++ test.c
@@ -3,18 +3,36 @@
 #include<math.h>
 
  double expected_value=50.0;
- double std_deviation=10.0;
+ double std_deviation=2.0;
  double amount_of_values = 0;
  int i;
  double dist_out;
+
  #define PI (3.1415)
 
+
+
  int main (void){
+
+        FILE *output_stream;
+        output_stream = fopen("output_stream", "rw"); //Read/Write
+
+        if (output_stream == NULL) // test for files not existing.
+            		{
+              		printf("Error! Could not open file\n");
+              		exit(-1); // must include stdlib.h
+                    }
+
         for(i=0;i<=100;i++){
 
                 //dist_out=amount_of_values;
                 dist_out = (1.0/(std_deviation*sqrt(2.0*PI)))*(exp(-1.0*((pow((amount_of_values - expected_value), 2.0))/(2.0*(pow(std_deviation, 2.0))))));
-                printf("%.10f \n",dist_out);
+
+
+                // write to file
+                fprintf(output_stream, "%f \n",dist_out); // write to file
+
+                printf("%f \n",dist_out);
                 amount_of_values=amount_of_values+1.0;
                 }
         return 0;
