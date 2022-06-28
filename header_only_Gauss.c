c5c519232b361e8457067b507715ad980e0c860f
Fabian Hilbich <fabian_hilbich@web.de>
2022-06-28 11:38:38 +0200

    Help im header only Programm angepasst. fopen Parameter von mode 'a+' zu 'w+' geändert. So wird die datei bei jedem ausführen neu beschrieben anstatt jedes mal erweitert zu werden.
    Aufgabe 2 für static Librarie begonnen.
    
    Fabian

--- header_only_Gauss.c
+++ header_only_Gauss.c
@@ -18,7 +18,9 @@ void help(void){
 
 	printf("Version 0.1 Apha \n");
 	printf("Autor: Fabian Hilbich, Camil Cieslok\n");
-	printf("Dieses Programm, stellt anhand der beim Programmstart, mitgegebenen Variablen die Normalverteilung nach Gauss Dar.\n");
+	printf("Dieses Programm, generiert mit Hilfe der beim Programmstart, übergebenen Variablen eine definierte Anzahl von Normalverteilten Zufallswerten nach Gauß.\n");
+	printf("Sollten Beim Progammstart keine Variablen übergeben worden sein, findet eine automatische Generierung der Zahlen mit den Default Werten statt. \n");
+	printf("Die Ausgabe erfolgt in die Konsole sowie in die Datei (output_stream.txt) im Programmverzeichnis. \n");
 	printf("Optionen: \n");
 	printf("-e	Erwartungswert (default = 10 \n");
 	printf("-s	Standardabweichung (default = 1) \n");
@@ -35,26 +37,25 @@ int main (int argc,char *argv[]){
     	{
     	    switch(option) 
         	{ 
-	case 'h':
-		help();
-	break;	
-	case 'e': 
-                expected_value=atoi(optarg);
-		printf("Erwartungswert: %i\n", expected_value); 
-        break; 
-        case 's': 
-               	std_deviation=atoi(optarg); 
-		printf("Standardabweichung: %i \n", std_deviation); 
-        break; 
-        case 'a': 
-                amount_of_values=atoi(optarg);
-		printf("Anzahl Werte: %i\n", amount_of_values);
-        break; 
-        }
-	}
-	
+			case 'h':
+				help();
+			break;	
+			case 'e': 
+						expected_value=atoi(optarg);
+				printf("Erwartungswert: %i\n", expected_value); 
+			break; 
+			case 's': 
+						std_deviation=atoi(optarg); 
+				printf("Standardabweichung: %i \n", std_deviation); 
+			break; 
+			case 'a': 
+						amount_of_values=atoi(optarg);
+				printf("Anzahl Werte: %i\n", amount_of_values);
+			break; 
+			}
+		}
         FILE *output_stream;
-        output_stream = fopen("output_stream.txt", "a+"); 
+        output_stream = fopen("output_stream.txt", "w+"); 
 
         if (output_stream == NULL) // test for files not existing.
             		{
--- makefile
+++ makefile
@@ -1,8 +1,11 @@
-application: 
+application_header_only: 
 	gcc-10 header_only_Gauss.c -o header_only_Gauss -lm
 
+application_static: staticlib 
+	gcc-10 calc_Gauss_stdDev_expected.c -o calc_Gauss_stdDev_expected -lGauss_staticlib -lm
+	
 staticlib:
-	gcc *PROGRAMM*.c -c
-	ar -crs lib*LIBNAME.a *LIBNAME*.o
-	sudo cp *LIBNAME*.h /usr/include
-	sudo cp lib*LIBNAME.a /usr/include
+	gcc Gauss_staticlib.c -c
+	ar -crs libGauss_staticlib.a Gauss_staticlib.o
+	sudo cp Gauss_staticlib.h /usr/include
+	sudo cp libGauss_staticlib.a /usr/include
